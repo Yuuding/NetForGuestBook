81f38d8b3a711c7200de5b08b546ca482593aafc
Sunshine7155 <67466828+Sunshine7155@users.noreply.github.com>
2020-11-26 20:23:27 +0800

    Add files via upload

---
+++ Scripts/myScript.js
@@ -0,0 +1,12 @@
+﻿var clickNum = 0;
+
+function showMenu() {
+    clickNum++;
+    var row = document.getElementById("opNav");
+    if (clickNum % 2 == 1)
+        row.style.height = "22px";
+    else
+        row.style.height = "0px";
+
+
+}
